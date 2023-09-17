9e726ee761329cc7fa342f6957a681d367118d13
Sparsh Mishra <sparsh.mishra@igniteconnex.com>
2023-09-17 12:13:54 +0530

    commited location

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  // Use useState to initialize userLocation as an object with null values
+  const [userLocation, setUserLocation] = useState({ latitude: null, longitude: null });
+
+  const getUserLocation = () => {
+    // Check if geolocation is available in the browser
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setUserLocation({ latitude, longitude });
+        },
+        (error) => {
+          console.error("Error getting user information:", error);
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Geolocation App</h1>
+      <button onClick={getUserLocation}>Get User Location</button>
+      {userLocation.latitude !== null && userLocation.longitude !== null && (
+        <div>
+          <h2>User Location</h2>
+          <p>Latitude: {userLocation.latitude}</p>
+          <p>Longitude: {userLocation.longitude}</p>
+        </div>
+      )}
     </div>
   );
 }
